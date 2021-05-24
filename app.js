168744a927b52e8b6fee08c0f276e40267c5b622
Anshum <anshumdutta5@gmail.com>
2021-05-24 12:57:34 +0530

    Added Notifications API

--- app.js
+++ app.js
@@ -28,52 +28,115 @@ mysqlConnection.connect(function(err) {
 
 
 
-
+//**********************************************************************Institute Details*************************************************************
 
 app.post("/Get_institute_details", function(req, res) {
 
-  var test={
-    title:[],
-    content:[]
+  var test = {
+    title: [],
+    content: []
   }
-var id = req.body.id;
-var response={};
-var result={
-  response:{},
-  status:""
-};
+  var id = req.body.id;
+  var response = {};
+  var result = {
+    response: {},
+    status: ""
+  };
 
-mysqlConnection.query("SELECT * FROM web_institute_detail WHERE inst_hash=?", id, function(err, rows){
-  if(err){
-    console.log(err);
-    result.status="failed";
-    res.send(result);
-  }else{
-    if (rows.length > 0){
-      var img="imgURL";
-      rows.forEach(function(row){
-        test.title.push(row.title);
-        test.content.push(row.content);
-      })
+  mysqlConnection.query("SELECT * FROM web_institute_detail WHERE inst_hash=?", id, function(err, rows) {
+    if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      if (rows.length > 0) {
+        var img = "imgURL";
+        rows.forEach(function(row) {
+          test.title.push(row.title);
+          test.content.push(row.content);
+        })
 
-      response[img]="https://careerliftprod.s3.amazonaws.com/mcllearnoadminimage/";
-      for( var i=0;i<test.title.length;i++){
-        response[test.title[i]]=test.content[i];
+        response[img] = "https://careerliftprod.s3.amazonaws.com/mcllearnoadminimage/";
+        for (var i = 0; i < test.title.length; i++) {
+          response[test.title[i]] = test.content[i];
+        }
+        result.response = response;
+        result.status = "Success";
+        res.send(result);
+      } else {
+        result.status = "failed";
+        res.send(result);
       }
-      result.response=response;
-      result.status="Success";
-      res.send(result);
-  }else{
-    result.status="failed";
-    res.send(result);
-  }
 
-}
-})
+    }
+  })
 });
 
 
 
-app.listen(3000, function() {
+//**********************************************************************Notifications*****************************************************************
+
+
+app.post("/Get_notification1", function(req, res) {
+
+
+  function NewRow(notify_id, notify_title, notify_description, date) {
+    this.notify_id = notify_id;
+    this.notify_title = notify_title;
+    this.notify_description = notify_description;
+    this.date = date;
+  }
+
+  var newRows = [];
+  var newRowsobj = {
+    response: newRows,
+    status: ""
+  }
+  var data = [];
+  var id = req.body.id;
+  var date;
+
+  mysqlConnection.query("SELECT * FROM web_notification WHERE inst_hash=?", id, function(err, rows) {
+    if (!err) {
+      //console.log(rows.length);
+      if (rows.length > 0) {
+
+
+        rows.forEach(function(row) {
+          //console.log(row);
+
+          if (row.notify_date_update === null || row.notify_date_update === "" || row.notify_date_update === '0000-00-00') {
+            date = row.notify_date;
+          } else {
+            date = row.notify_date_update;
+          }
+
+
+          var newRow = new NewRow(row.notify_id, row.notify_title, row.notify_description, date);
+          newRows.push(newRow);
+          //res.send(newRow);
+        })
+        //console.log("Success");
+        newRowsobj.status = "success"
+
+        res.send(newRowsobj);
+      } else {
+        newRowsobj.status = "failed"
+        res.send(newRowsobj);
+      }
+
+    } else {
+      console.log(err);
+      res.send(err);
+      //res.send(newRow);
+    }
+
+  })
+});
+
+
+
+
+
+app.listen(process.env.PORT, function() {
   console.log("Server started on port 3000");
 })
